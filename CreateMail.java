5c51837d7cd08e7df0f9d049022b49f226788980
Johnjoe Landers <johnjoe.landers@students.ittralee.ie>
2014-12-08 09:14:46 +0000

    Added a textfield

--- CreateMail.java
+++ CreateMail.java
@@ -1,3 +1,6 @@
+//CreateMail.java
+//johnjoe landers t00174319
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -6,14 +9,22 @@ public class CreateMail extends JFrame{
 		JLabel item1;
 		JLabel item2;
 		JButton button;
+		JTextField textField1;
+		JTextField textField2;
 		
 		public void CreateMail(){
 			
-			JLabel item1 = new JLabel("To: ");
-			add(item1);
+			JLabel label1 = new JLabel("To: ");
+			add(label1);
+			
+			JTextField textField1 =new JTextField(20);
+			add(textField1);
 				
-			JLabel item2 = new JLabel("Enter message here: ");
-			add(item2);
+			JLabel label2 = new JLabel("Enter message here: ");
+			add(label1);
+			
+			JTextField textField2 =new JTextField(40);
+			add(textField2);
 				
 			JButton button = new JButton("Enter");
 			add(button);
